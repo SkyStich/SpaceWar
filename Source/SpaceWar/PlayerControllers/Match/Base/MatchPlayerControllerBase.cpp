a2407ebf1e6f89d5f49a698f1cae003e89adee6c
Stich <SkyStich@users.noreply.github.com>
2021-04-28 01:29:48 +0300

    Create respawn character

---
+++ Source/SpaceWar/PlayerControllers/Match/Base/MatchPlayerControllerBase.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MatchPlayerControllerBase.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Net/UnrealNetwork.h"
+#include "UObject/ConstructorHelpers.h"
+#include "SpaceWar/SpaceWarCharacter.h"
+#include "SpaceWar/GameModes/Match/Base/MatchGameModeBase.h"
+
+AMatchPlayerControllerBase::AMatchPlayerControllerBase()
+{
+	bReplicates = true;
+	NetUpdateFrequency = 5.f;
+	bCanSpawn = true;
+
+	static ConstructorHelpers::FClassFinder<ASpaceWarCharacter> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if(PlayerPawnBPClass.Class != nullptr)
+	{
+		PlayerClass = PlayerPawnBPClass.Class;
+	}
+}
+
+void AMatchPlayerControllerBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AMatchPlayerControllerBase, PlayerClass);
+	DOREPLIFETIME_CONDITION(AMatchPlayerControllerBase, bCanSpawn, COND_OwnerOnly);
+}
+
+void AMatchPlayerControllerBase::SetPlayerClass(TSubclassOf<ASpaceWarCharacter> NewPlayerClass)
+{
+	if(GetLocalRole() == ROLE_Authority)
+	{
+		PlayerClass = NewPlayerClass;
+	}
+}
+
+void AMatchPlayerControllerBase::SpawnPlayer()
+{
+	if(!bCanSpawn) return;
+
+	ASpaceWarCharacter* CharacterForSpawn;
+	auto const GM = Cast<AMatchGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
+	if(GM)
+	{
+		GM->SpawnCharacter(this, CharacterForSpawn);
+		bCanSpawn = false;
+	}	
+}
+
+void AMatchPlayerControllerBase::LaunchRespawnTimer(float const Time)
+{
+	FTimerDelegate TimerDel;
+	TimerDel.BindLambda([&]() -> void
+	{
+		bCanSpawn = true;
+	});
+	GetWorld()->GetTimerManager().SetTimer(RespawnTimer, TimerDel, Time, false);
+}
+
+void AMatchPlayerControllerBase::Server_SpawnPlayerPressed_Implementation()
+{
+	GetWorld()->GetTimerManager().ClearTimer(RespawnTimer);
+	SpawnPlayer();
+}
+
+void AMatchPlayerControllerBase::SetupInputComponent()
+{
+	if(!InputComponent)
+	{
+		InputComponent = NewObject<UInputComponent>(this);
+		InputComponent->RegisterComponent();
+	}
+
+	InputComponent->BindAction("SpawnPlayer", IE_Released, this, &AMatchPlayerControllerBase::Server_SpawnPlayerPressed);
+}
+
---
+++ Source/SpaceWar/PlayerControllers/Match/Base/MatchPlayerControllerBase.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MatchPlayerControllerBase.generated.h"
+
+class ASpaceWarCharacter;
+
+UCLASS(Blueprintable)
+class SPACEWAR_API AMatchPlayerControllerBase : public APlayerController
+{
+	GENERATED_BODY()
+	
+	UFUNCTION(Server, Unreliable)
+	void Server_SpawnPlayerPressed();
+
+public:
+
+	AMatchPlayerControllerBase();
+
+	void SetPlayerClass(TSubclassOf<ASpaceWarCharacter> NewPlayerClass);
+
+	void SpawnPlayer();
+	void LaunchRespawnTimer(float const Time);
+
+	UFUNCTION(BlueprintPure, Category = "PlayerController|PlayerClass")
+	TSubclassOf<ASpaceWarCharacter> GetPlayerClass() const { return PlayerClass; }
+
+protected:
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void SetupInputComponent() override;
+
+private:
+
+	UPROPERTY(Replicated)
+	TSubclassOf<ASpaceWarCharacter>PlayerClass;
+	
+	UPROPERTY(Replicated)
+	bool bCanSpawn;
+
+	FTimerHandle RespawnTimer;
+};
