bc894479aeef99c8e1352cacabcf13cf3257bb80
Stich <SkyStich@users.noreply.github.com>
2021-04-28 01:27:28 +0300

    Update SpaceWar.Build.cs

--- Source/SpaceWar/SpaceWar.Build.cs
+++ Source/SpaceWar/SpaceWar.Build.cs
@@ -8,6 +8,6 @@ public class SpaceWar : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG" });
 	}
 }
