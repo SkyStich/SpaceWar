e12ed7ab62a2883f90787728da1fccc50e5e8bbc
Stich <SkyStich@users.noreply.github.com>
2021-07-19 17:29:43 +0300

    Add assets

---
+++ Source/SpaceWar/Pawns/Match/SpecialWeapon/AmmunitionSpecialBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Match/SpecialWeapon/SpecialWeaponObjectBase.h"
+#include "AmmunitionSpecialBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEWAR_API AAmmunitionSpecialBase : public ASpecialWeaponObjectBase
+{
+	GENERATED_BODY()
+	
+};
