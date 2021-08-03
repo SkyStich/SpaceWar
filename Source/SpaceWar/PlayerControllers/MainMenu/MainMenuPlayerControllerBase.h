8a3b19b72309e514a9f078e3446427b80d403743
Stich <SkyStich@users.noreply.github.com>
2021-08-04 01:56:50 +0300

    Create main menu controller

---
+++ Source/SpaceWar/PlayerControllers/MainMenu/MainMenuPlayerControllerBase.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SpaceWar/Structs/UserInfo.h"
+#include "MainMenuPlayerControllerBase.generated.h"
+
+UCLASS()
+class SPACEWAR_API AMainMenuPlayerControllerBase : public APlayerController
+{
+	GENERATED_BODY()
+
+};
