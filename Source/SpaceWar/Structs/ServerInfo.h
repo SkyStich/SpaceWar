c33b0b96c2a8d11c8942862bca02b6a761710cd8
Stich <SkyStich@users.noreply.github.com>
2021-08-11 18:28:36 +0300

    add server info delegate

--- Source/SpaceWar/Structs/ServerInfo.h
+++ Source/SpaceWar/Structs/ServerInfo.h
@@ -10,9 +10,28 @@ struct FServersData
 {
 	GENERATED_BODY()
 
+	FServersData() : Id(-1), Name(""), Address("") {}
+	
+	UPROPERTY(BlueprintReadOnly)
+	int32 Id;
+	
 	UPROPERTY(BlueprintReadOnly)
 	FString Name;
 
 	UPROPERTY(BlueprintReadOnly)
 	FString Address;
 };
+
+DECLARE_DYNAMIC_DELEGATE_ThreeParams(FGetServerInfoDelegate, bool, bResult, const FString&, ErrorMessage, const FServersData&, ServerData);
+
+USTRUCT(BlueprintType)
+struct FGetServerInfoCallBack
+{
+	GENERATED_BODY()
+
+	FGetServerInfoCallBack() : OnGetServerInfoDelegate(FGetServerInfoDelegate()) {}
+	FGetServerInfoCallBack(const FGetServerInfoDelegate& CallBack) : OnGetServerInfoDelegate(CallBack) {}
+	
+	FGetServerInfoDelegate OnGetServerInfoDelegate;
+};
+
