0a696f58c5954d2ab689434e168de70518337d40
Stich <SkyStich@users.noreply.github.com>
2021-08-12 04:18:55 +0300

    Update GameServerDataBaseComponent.cpp

--- Source/SpaceWar/Components/GameMode/GameServerDataBaseComponent.cpp
+++ Source/SpaceWar/Components/GameMode/GameServerDataBaseComponent.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "GameServerDataBaseComponent.h"
 #include "GameFramework/GameModeBase.h"
 #include "Kismet/GameplayStatics.h"
@@ -128,6 +127,7 @@ void UGameServerDataBaseComponent::RemoveServerFromDataBase()
 
 void UGameServerDataBaseComponent::OnResponseRemoveServerFromDataBase(bool bResult, const FString& ErrorMessage)
 {
+	ShutDownServer();
 	UE_LOG(LogTemp, Warning, TEXT("OnResponseRemoveServerFromDataBase"));
 }
 
