7f8f35b0cc8cc676781056a827a820ecb4a3eb99
Stich <SkyStich@users.noreply.github.com>
2021-08-12 04:18:50 +0300

    Create RemoveServerFromDataBase.php

---
+++ PHPRequest/RemoveServerFromDataBase.php
@@ -0,0 +1,19 @@
+<?php
+
+	include_once 'ConnectToDataBase.php';
+
+	$Data = json_decode(file_get_contents('php://input'));
+	$id = $Data->id;
+
+	$stmt = $Connect->prepare('Delete From servers where servers.ServerId = ?');
+	$stmt->bind_param("s", $id);
+	$stmt->execute();
+
+	if($stmt->fetch())
+	{
+		echo json_encode(array('Result' => true));
+	}
+	else
+	{
+		echo json_encode(array('Result' => false, 'Error' => 'The server could not be deleted. There is no server with this id'));
+	}
